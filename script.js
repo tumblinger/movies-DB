bc0cfa2f3d70e378a8990dc70f25a187f5eff5cf
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-04-08 20:26:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const api_url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3b8fd241768fc301f39b376c4fe192e2&page=1&language=en'
 const img_path = 'https://image.tmdb.org/t/p/w1280'
-const search_api = 'https://api.themoviedb.org/3/search/movie?api_key=3b8fd241768fc301f39b376c4fe192e2&language=ru&query="'
+const search_api = 'https://api.themoviedb.org/3/search/movie?api_key=3b8fd241768fc301f39b376c4fe192e2&language=en&query="'
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 const main = document.getElementById('main')
